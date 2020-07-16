b44d829f7c3d14b70f98eea7c56509c90bd35085
Jonas Bak Phillipson <jbakchr@gmail.com>
2020-07-17 00:55:17 +0200

    New creds added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import time
-from datetime import datetime
+import datetime
 import base64
 import email
 import pickle
@@ -11,6 +11,8 @@ from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://mail.google.com/']
+
+CALENDAR_SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 
 def get_gmail_service():
@@ -76,9 +78,64 @@ def get_books_return_date(service, user, msgId):
         print("An error occured while trying to fetch message")
 
 
+def get_calendar_service():
+
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('calendar-token.pickle'):
+        with open('calendar-token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', CALENDAR_SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('calendar-token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    return build('calendar', 'v3', credentials=creds)
+
+
+def create_cal_event(service, furtureDate, nextDay):
+    # Specify event
+    event = {
+        'summary': 'Afl. biblioteksbøger',
+        'start': {
+            'dateTime': furtureDate,
+            'timeZone': 'Europe/Copenhagen',
+        },
+        'end': {
+            'dateTime': nextDay,
+            'timeZone': 'Europe/Copenhagen',
+        },
+    }
+
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print(event.get('htmlLink'))
+
+
 gmail_service = get_gmail_service()
 message_id = get_latest_message_id(gmail_service, 'me')
 
 if message_id != None:
+    # Get book return date from message
     book_return_date = get_books_return_date(gmail_service, 'me', message_id)
-    print(type(book_return_date))
+
+    # Get calendar service
+    calender_service = get_calendar_service()
+
+    # Create future dates
+    return_date_obj = datetime.datetime(
+        int(book_return_date[6:]), int(book_return_date[3:5]), int(book_return_date[:2]))
+    future_return_date = return_date_obj + datetime.timedelta(days=30)
+    nextDay = future_return_date + datetime.timedelta(days=1)
+
+    # Create calendar event
+    create_cal_event(calender_service, str(
+        future_return_date.isoformat()), str(nextDay.isoformat()))
